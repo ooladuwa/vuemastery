1596a818f639f7b68157b79b1e4831d4f43754ed
Onaje Oladuwa <onaje.oladuwa@groundfloor.us>
2024-10-16 08:48:37 -0400

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-console.log(supabase)
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between p-6 items-center">
+    <form w-full max-w-96>
+      <input
+        type="text"
+        placeholder="Search..."
+        class="w-full pl-8 bg-background"
+      />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
